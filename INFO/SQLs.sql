a506897e762c416c21102396581468d13a438afc
Oleksiy Bechke <bechke@dlit.dp.ua>
2024-11-12 01:21:16 +0200

    Initial commit

---
+++ INFO/SQLs.sql
@@ -0,0 +1,12 @@
+
+CREATE DATABASE contact_db;
+
+CREATE TABLE IF NOT EXISTS contacts
+( id BIGINT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  phone VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+
